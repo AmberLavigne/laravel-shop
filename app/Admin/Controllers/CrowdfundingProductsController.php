4421f3135665010c1fecd284c4fc58d8b8279d28
Yu jiaxing <yujiaxing@chicv.com>
2019-02-15 01:39:14 +0000

    4.3后台众筹商品管理

--- app/Admin/Controllers/CrowdfundingProductsController.php
+++ app/Admin/Controllers/CrowdfundingProductsController.php
@@ -11,8 +11,9 @@ use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 
 use App\Models\Category;
+use App\Models\CrowdfundingProduct;
 
-class ProductsController extends Controller
+class CrowdfundingProductsController extends Controller
 {
     use HasResourceActions;
 
@@ -25,8 +26,8 @@ class ProductsController extends Controller
     public function index(Content $content)
     {
         return $content
-            ->header('商品列表')
-            ->description('...')
+            ->header('众筹商品列表')
+            ->description('description')
             ->body($this->grid());
     }
 
@@ -55,7 +56,7 @@ class ProductsController extends Controller
     public function edit($id, Content $content)
     {
         return $content
-            ->header('编辑商品')
+            ->header('编辑众筹商品')
             ->description('description')
             ->body($this->form()->edit($id));
     }
@@ -69,7 +70,7 @@ class ProductsController extends Controller
     public function create(Content $content)
     {
         return $content
-            ->header('创建商品')
+            ->header('创建众筹商品')
             ->description('description')
             ->body($this->form());
     }
@@ -82,35 +83,33 @@ class ProductsController extends Controller
     protected function grid()
     {
         $grid = new Grid(new Product);
-        $grid->model()->where('type',Product::TYPE_NORMAL)->with(['category']);
+        $grid->model()->where('type',Product::TYPE_CROWDFUNDING);
+        $grid->id('ID')->sortable();
 
-        $grid->id('Id')->sortable();
-        $grid->column('category.name',  '类目');
         $grid->title('商品名称');
-        $grid->description('Description')->display(function($description){
-            return str_limit($description,20,'...');
-        });
-        $grid->image('Image')->display(function($image){
-            return "<img  src='{$image}' class='thumbnail' style='max-width: 30px;' />";
-        });
         $grid->on_sale('已上架')->display(function($value){
             return $value ? '是' : '否';
         });
+
         $grid->price('价格');
-        $grid->rating('评分');
-        $grid->sold_count('销量');
-        $grid->review_count('评论数');
+
+        $grid->column('crowdfunding.target_amount', '目标金额');
+        $grid->column('crowdfunding.end_at', '结束时间');
+        $grid->column('crowdfunding.total_amount', '目前金额');
+
+        $grid->column('crowdfunding.status', ' 状态')->display(function($value){
+            return CrowdfundingProduct::$statusMap[$value];
+        });
         $grid->actions(function($actions){
             $actions->disableView();
             $actions->disableDelete();
         });
-        #tools 工具  batch 批次
+
         $grid->tools(function($tools){
             $tools->batch(function($batch){
-                // $batch->disableDelete();
+                $batch->disableDelete();
             });
         });
-
         return $grid;
     }
 
@@ -125,6 +124,8 @@ class ProductsController extends Controller
         $show = new Show(Product::findOrFail($id));
 
         $show->id('Id');
+        $show->type('Type');
+        $show->category_id('Category id');
         $show->title('Title');
         $show->description('Description');
         $show->image('Image');
@@ -147,33 +148,33 @@ class ProductsController extends Controller
     protected function form()
     {
         $form = new Form(new Product);
-        $form->hidden('type')->value(Product::TYPE_NORMAL);
+        $form->hidden('type')->value(Product::TYPE_CROWDFUNDING);
         $form->text('title', '商品名称')->rules('required');
-
         $form->select('category_id', '类目')->options(function($id){
             $category = Category::find($id);
 
             if ($category){
-
                 return [$category->id => $category->full_name];
             }
         })->ajax('/admin/api/categories?is_directory=0');
+        $form->image('image', '封面图片' )->rules('required|image');
 
-
-        $form->image('image', '封面图片')->rules('required|image');
         $form->editor('description', '商品描述')->rules('required');
-       
-        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default(0);
-        $form->hasMany('skus', 'SKU 列表' , function(Form\NestedForm $form){ #Nested 嵌套的
+        $form->radio('on_sale','上架')->options(['1' => '是', '0' => '否'])->default(0);
+
+        $form->text('crowdfunding.target_amount', '众筹目标金额')->rules('required|numeric|min:0.01');
+        $form->datetime('crowdfunding.end_at', '众筹结束时间')->rules('required|date');
+
+        $form->hasMany('skus',function(Form\NestedForm  $form){
             $form->text('title', 'SKU 名称')->rules('required');
             $form->text('description', 'SKU 描述')->rules('required');
             $form->text('price', '单价')->rules('required|numeric|min:0.01');
             $form->text('stock', '剩余库存')->rules('required|integer|min:0');
         });
-        $form->saving(function(Form $form){
-            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price')? : 0;
-        });
 
+        $form->saving(function(Form $form){
+            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price');
+        });
         return $form;
     }
 }
--- back_admin_db.sh
+++ back_admin_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 导入 .env  环境变量
-source .env
+source ./.env
 # 要备份的表
 tables="admin_menu admin_permissions admin_role_menu admin_role_permissions admin_role_users admin_roles admin_user_permissions admin_users"
 # 执行备份
