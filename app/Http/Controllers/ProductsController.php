dcefecf76b707a29c75fdcdf7b22b8df9304b9a2
Yu jiaxing <yujiaxing@chicv.com>
2019-03-22 09:48:43 +0000

    7.5封装 Elasticsearch 查询代码

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,6 +8,7 @@ use App\Models\Product;
 use App\Models\OrderItem;
 use App\Exceptions\InvalidRequestException;
 use Illuminate\Pagination\LengthAwarePaginator;
+use App\SearchBuilders\ProductSearchBuilder;
 
 class ProductsController extends Controller
 {
@@ -54,7 +55,7 @@ class ProductsController extends Controller
             ]);
     }
 
-    public function index(Request $request)
+    public function index_back1(Request $request)
     {
         $page    = $request->input('page', 1);
         $perPage = 16;
@@ -201,6 +202,84 @@ class ProductsController extends Controller
         ]);
     }
 
+    public function index(Request $request)
+    {
+        $page    = $request->input('page', 1);
+        $perPage = 16;
+        // 新建查询构造器对象，设置只搜索上架商品，设置分页
+        $builder = (new ProductSearchBuilder())->onSale()->paginate($perPage, $page);
+
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            // 调用查询构造器的类目筛选
+            $builder->category($category);
+        }
+
+        if ($search = $request->input('search', '')) {
+            $keywords = array_filter(explode(' ', $search));
+            // 调用查询构造器的关键词筛选
+            $builder->keywords($keywords);
+        }
+
+        if ($search || isset($category)) {
+            // 调用查询构造器的分面搜索
+            $builder->aggregateProperties();
+        }
+
+        $propertyFilters = [];
+        if ($filterString = $request->input('filters')) {
+            $filterArray = explode('|', $filterString);
+            foreach ($filterArray as $filter) {
+                list($name, $value) = explode(':', $filter);
+                $propertyFilters[$name] = $value;
+                // 调用查询构造器的属性筛选
+                $builder->propertyFilter($name, $value);
+            }
+        }
+        if ($order = $request->input('order', '')) {
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 调用查询构造器的排序
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+        $result = app('es')->search($builder->getParams());
+        // 通过 collect 函数将返回结果转为集合，并通过集合的 pluck 方法取到返回的商品 ID 数组
+        $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
+        // 通过 whereIn 方法从数据库中读取商品数据
+        $products = Product::query()
+            ->whereIn('id', $productIds)
+            ->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $productIds)))
+            ->get();
+        // 返回一个 LengthAwarePaginator 对象
+        $pager = new LengthAwarePaginator($products, $result['hits']['total'], $perPage, $page, [
+            'path' => route('products.index', false), // 手动构建分页的 url
+        ]);
+        $properties = [];
+        // 如果返回结果里有 aggregations 字段，说明做了分面搜索
+        if (isset($result['aggregations'])) {
+            $properties = collect($result['aggregations']['properties']['properties']['buckets'])
+                ->map(function($bucket){
+                    return [
+                        'key' => $bucket['key'],
+                        'values' => collect($bucket['value']['buckets'])->pluck('key')->all()
+                    ];
+                })->filter(function ($property) use($propertyFilters){
+                    return count($property['values']) >1 && !isset($propertyFilters[$property['key']]);
+                });
+        }
+        return view('products.index', [
+            'products' => $pager,
+            'filters'  => [
+                'search' => $search,
+                'order'  => $order,
+            ],
+            'category' => $category ?? null,
+            'properties' => $properties,
+            'propertyFilters' => $propertyFilters,
+        ]);
+
+    }
     public function show(Product $product, Request $request){
     	if (! $product->on_sale) {
     		throw new InvalidRequestException("商品未上架");	
